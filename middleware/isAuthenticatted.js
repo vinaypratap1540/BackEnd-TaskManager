160113cda779297892c12e9d1a2b4a4476be20ff
Vinay Pratap Singh Sirohiya <123262231+vinaypratap1540@users.noreply.github.com>
2025-03-07 16:22:46 +0530

    Update isAuthenticatted.js

--- middleware/isAuthenticatted.js
+++ middleware/isAuthenticatted.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken"
 
 const isAuthenticated = async(req,res,next)=>{
    try {
+    console.log("Cookies received:", req.cookies); // Debugging
     const token = req.cookies.token
     if(!token){
         return res.status(401).json({
@@ -19,7 +20,8 @@ const isAuthenticated = async(req,res,next)=>{
     req.id=decode.userId
     next()
    } catch (error) {
-    console.log(error)
+    console.log("Authentication Error:", error); // Debugging
+     res.status(500).json({ success: false, message: "Server error" });
    }
 }
 
